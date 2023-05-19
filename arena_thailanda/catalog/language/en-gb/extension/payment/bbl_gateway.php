41b21b359c00fae23a1ac631fa7a87629dbc3dcf
charoenlap <friendlysoftpro@gmail.com>
2023-05-19 12:41:09 +0700

    update

--- arena_thailanda/catalog/language/en-gb/extension/payment/bbl_gateway.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_title']       = 'Visa/MasterCard';
-$_['text_payment']     = 'Payment via Visa/MasterCard';
--- arena_thailanda/catalog/language/th/tocex/toc_footerextra.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['text_contactus']   = 'ติดต่อเรา';
-$_['text_address']      =  'ที่ตั้งร้านค้า';
-$_['text_about']        = 'เกี่ยวกับเรา';  
-?>
